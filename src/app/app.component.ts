f99b872033756788908d8937a446425f25e8a106
Mohit Kataria <mohit.kataria@pretpick.com>
2020-06-20 11:39:54 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from "@angular/core";
+import * as jsonData from "src/assets/json/colleges.json";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'frontend-task';
+export class AppComponent implements OnInit {
+  title = "frontend-task";
+  collegeData = [];
+  index = 0;
+
+  ngOnInit() {
+    console.log(jsonData["default"]);
+    this.loadData();
+  }
+
+  loadData() {
+    if (this.index < jsonData["default"].colleges.length) {
+      for (let i = this.index; i < this.index + 10; i++) {
+        this.collegeData.push(jsonData["default"]["colleges"][i]);
+      }
+      console.log(this.collegeData);
+    }
+  }
+
+  @HostListener("window:scroll", ["$event"])
+  scroll(event) {
+    // console.log(event.path);
+    if (
+      event.path[1].innerHeight + event.path[1].scrollY >=
+      event.path[0].body.scrollHeight
+    ) {
+      this.index += 10;
+      this.loadData();
+    }
+  }
 }
