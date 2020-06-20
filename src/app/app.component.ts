97344d3994797fcfee029cbcadcefde08b50c330
Mohit Kataria <mohit.kataria@pretpick.com>
2020-06-20 15:36:05 +0530

    final-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,9 @@ import * as jsonData from "src/assets/json/colleges.json";
 export class AppComponent implements OnInit {
   title = "frontend-task";
   collegeData = [];
-  index = 0;
+  index: number = 0;
 
   ngOnInit() {
-    console.log(jsonData["default"]);
     this.loadData();
   }
 
@@ -21,13 +20,12 @@ export class AppComponent implements OnInit {
       for (let i = this.index; i < this.index + 10; i++) {
         this.collegeData.push(jsonData["default"]["colleges"][i]);
       }
-      console.log(this.collegeData);
+      // console.log(this.collegeData);
     }
   }
 
   @HostListener("window:scroll", ["$event"])
   scroll(event) {
-    // console.log(event.path);
     if (
       event.path[1].innerHeight + event.path[1].scrollY >=
       event.path[0].body.scrollHeight
